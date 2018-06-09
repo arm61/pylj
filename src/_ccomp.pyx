ff98efb0ea840403ecac4dcdb22041d2381b8348
arm61 <arm61@bath.ac.uk>
2018-06-09 12:24:12 +0100

    adding energy sampling to the molecular dynamics modules

--- src/_ccomp.pyx
+++ src/_ccomp.pyx
@@ -5,7 +5,7 @@ cimport numpy as np
 
 cdef extern from "comp.h":
     void compute_accelerations(int len_particles, const double *xpos, const double *ypos, double *xacc, double *yacc,
-                               double *distances_arr, double box_l, double *force_arr)
+                               double *distances_arr, double box_l, double *force_arr, double *energy_arr)
     void compute_energies(int len_particles, const double *xpos, const double *ypos, double *distances_arr, double box_l,
                         double *energy_arr)
     double compute_pressure(int number_of_particles, const double *xvel, const double *yvel, double box_length,
@@ -16,7 +16,7 @@ cdef extern from "comp.h":
 DTYPE = np.float64
 ctypedef np.float64_t DTYPE_t
 
-def compute_forces(particles, distances, forces, box_length):
+def compute_forces(particles, box_length):
     """Calculates the forces and therefore the accelerations on each of the particles in the simulation. This uses a
     12-6 Lennard-Jones potential model for Argon with values:
 
@@ -29,10 +29,6 @@ def compute_forces(particles, distances, forces, box_length):
         Information about the particles.
     box_length: float
         Length of a single dimension of the simulation square, in Angstrom.
-    distances: float, array_like
-        Old distances between each of the pairs of particles in the simulation.
-    forces: float, array_like
-        Old forces between each of the pairs of particles in the simulation.
 
     Returns
     -------
@@ -42,15 +38,19 @@ def compute_forces(particles, distances, forces, box_length):
         Current distances between pairs of particles in the simulation.
     float, array_like
         Current forces between pairs of particles in the simulation.
+    float, array_like
+        Current energies between pairs of particles in the simulation.
     """
     cdef int len_particles = particles['xposition'].size
+    pairs = int((len_particles - 1) * len_particles / 2)
     cdef double box_l = box_length
     cdef np.ndarray[DTYPE_t, ndim=1] xpos = np.zeros(len_particles)
     cdef np.ndarray[DTYPE_t, ndim=1] ypos = np.zeros(len_particles)
     cdef np.ndarray[DTYPE_t, ndim=1] xacc = np.zeros(len_particles)
     cdef np.ndarray[DTYPE_t, ndim=1] yacc = np.zeros(len_particles)
-    cdef np.ndarray[DTYPE_t, ndim=1] distances_arr = np.zeros(len(distances))
-    cdef np.ndarray[DTYPE_t, ndim=1] force_arr = np.zeros(len(distances))
+    cdef np.ndarray[DTYPE_t, ndim=1] distances_arr = np.zeros(pairs)
+    cdef np.ndarray[DTYPE_t, ndim=1] force_arr = np.zeros(pairs)
+    cdef np.ndarray[DTYPE_t, ndim=1] energy_arr = np.zeros(pairs)
 
     for i in range(0, len_particles):
         xpos[i] = particles['xposition'][i]
@@ -59,16 +59,15 @@ def compute_forces(particles, distances, forces, box_length):
         yacc[i] = 0
 
     compute_accelerations(len_particles, <const double*>xpos.data, <const double*>ypos.data, <double*>xacc.data,
-                          <double*>yacc.data, <double*>distances_arr.data, box_l, <double*>force_arr.data)
+                          <double*>yacc.data, <double*>distances_arr.data, box_l, <double*>force_arr.data,
+                          <double*>energy_arr.data)
 
     for i in range(0, len_particles):
         particles['xacceleration'][i] = xacc[i]
         particles['yacceleration'][i] = yacc[i]
 
-    distances = distances_arr
-    forces = force_arr
 
-    return particles, distances, forces
+    return particles, distances_arr, force_arr, energy_arr
 
 def compute_energy(particles, distances, energies, box_length):
     """Calculates the total energy of the simulation. This uses a
