9db8af8edb10e3b4ec05d37eead84d92dc382a98
arm61 <arm61@bath.ac.uk>
2018-06-15 15:10:15 +0100

    adding no cover to cite and version functions

--- pylj/__init__.py
+++ pylj/__init__.py
@@ -1,11 +1,11 @@
 import webbrowser
 
 
-def __cite__():
+def __cite__(): #pragma: no cover
     webbrowser.open('https://zenodo.org/badge/latestdoi/119863480')
 
 
-def __version__():
+def __version__(): #pragma: no cover
     major = 0
     minor = 0
     micro = 22
