ee4a7a53845505fef371edb34abf07ac774221cc
arm61 <arm61@bath.ac.uk>
2018-08-02 17:09:11 +0100

    add path import

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ MICRO = 1
 ISRELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
+from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
