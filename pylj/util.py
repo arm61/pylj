c1bc3b759f9fb0cdbc13b0eeb268516075753338
arm61 <arm61@bath.ac.uk>
2018-09-19 14:31:02 +0100

    test appveyor

--- pylj/util.py
+++ pylj/util.py
@@ -231,7 +231,7 @@ def __version__():  # pragma: no cover
     """This will print the number of the pylj version currently in use."""
     major = 1
     minor = 1
-    micro = 5
+    micro = 6
     print('pylj-{:d}.{:d}.{:d}'.format(major, minor, micro))
 
 
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ packages = find_packages()
 # versioning
 MAJOR = 1
 MINOR = 1
-MICRO = 5
+MICRO = 6
 ISRELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
