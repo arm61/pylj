646cb488b647fea237685153d37e7fd99ddeb3e7
arm61 <arm61@bath.ac.uk>
2018-12-02 10:11:08 +0000

    version number bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ author = 'Andrew R. McCluskey'
 # built documents.
 #
 # The short X.Y version.
-version = '1.1.15'
+version = '1.1.16'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ packages = find_packages()
 # versioning
 MAJOR = 1
 MINOR = 1
-MICRO = 15
+MICRO = 16
 ISRELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
