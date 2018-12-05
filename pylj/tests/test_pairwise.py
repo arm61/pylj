9bc95dc8fb2774eac75d19397e9f263ab27eeb41
symmy596 <ars44@bath.ac.uk>
2018-12-05 12:34:59 +0000

    Formatting

--- pylj/tests/test_pairwise.py
+++ pylj/tests/test_pairwise.py
@@ -33,7 +33,8 @@ class TestPairwise(unittest.TestCase):
         particles['xposition'][0] = 1e-10
         particles['xposition'][1] = 5e-10
         particles, distances, forces, energies = pairwise.compute_force(
-                particles, 30, 15, constants=[1.363e-134, 9.273e-78], forcefield=ff.lennard_jones, mass=39.948)
+                particles, 30, 15, constants=[1.363e-134, 9.273e-78],
+                forcefield=ff.lennard_jones, mass=39.948)
         assert_almost_equal(distances, [4e-10])
         assert_almost_equal(energies, [-1.4515047e-21])
         assert_almost_equal(forces, [-9.5864009e-12])
@@ -46,17 +47,21 @@ class TestPairwise(unittest.TestCase):
         particles = np.zeros(2, dtype=part_dt)
         particles['xposition'][0] = 1e-10
         particles['xposition'][1] = 5e-10
-        distances, energies = pairwise.compute_energy(particles, 30, 15, constants=[1.363e-134, 9.273e-78], forcefield=ff.lennard_jones)
-        assert_almost_equal(distances, [4e-10])
-        assert_almost_equal(energies, [-1.4515047e-21])
+        d, e = pairwise.compute_energy(particles, 30, 15,
+                                       constants=[1.363e-134, 9.273e-78],
+                                       forcefield=ff.lennard_jones)
+        assert_almost_equal(d, [4e-10])
+        assert_almost_equal(e, [-1.4515047e-21])
 
     def test_calculate_pressure(self):
         part_dt = util.particle_dt()
         particles = np.zeros(2, dtype=part_dt)
         particles['xposition'][0] = 1e-10
         particles['xposition'][1] = 5e-10
-        pressure = pairwise.calculate_pressure(particles, 30, 300, 15, constants=[1.363e-134, 9.273e-78], forcefield=ff.lennard_jones)
-        assert_almost_equal(pressure*1e24, 7.07368869)
+        p = pairwise.calculate_pressure(particles, 30, 300, 15,
+                                        constants=[1.363e-134, 9.273e-78],
+                                        orcefield=ff.lennard_jones)
+        assert_almost_equal(p*1e24, 7.07368869)
 
     def test_pbc_correction(self):
         a = pairwise.pbc_correction(1, 10)
--- pylj/tests/test_util.py
+++ pylj/tests/test_util.py
@@ -6,7 +6,9 @@ import unittest
 
 class TestUtil(unittest.TestCase):
     def test_system_square(self):
-        a = util.System(2, 300, 8, mass=39.948, constants=[1.363e-134, 9.273e-78], forcefield=ff.lennard_jones)
+        a = util.System(2, 300, 8, mass=39.948,
+                        constants=[1.363e-134, 9.273e-78],
+                        forcefield=ff.lennard_jones)
         assert_equal(a.number_of_particles, 2)
         assert_equal(a.init_temp, 300)
         assert_almost_equal(a.box_length * 1e10, 8)
@@ -21,7 +23,9 @@ class TestUtil(unittest.TestCase):
         assert_equal(a.energies.size, 1)
 
     def test_system_random(self):
-        a = util.System(2, 300, 8, init_conf='random', mass=39.948, constants=[1.363e-134, 9.273e-78], forcefield=ff.lennard_jones)
+        a = util.System(2, 300, 8, init_conf='random', mass=39.948,
+                        constants=[1.363e-134, 9.273e-78],
+                        forcefield=ff.lennard_jones)
         assert_equal(a.number_of_particles, 2)
         assert_equal(a.init_temp, 300)
         assert_almost_equal(a.box_length * 1e10, 8)
@@ -37,21 +41,27 @@ class TestUtil(unittest.TestCase):
 
     def test_system_too_big(self):
         with self.assertRaises(AttributeError) as context:
-            util.System(2, 300, 1000, mass=39.948, constants=[1.363e-134, 9.273e-78], forcefield=ff.lennard_jones)
+            util.System(2, 300, 1000, mass=39.948,
+                        constants=[1.363e-134, 9.273e-78],
+                        forcefield=ff.lennard_jones)
         self.assertTrue('With a box length of 1000 the particles are probably '
                         'too small to be seen in the viewer. Try something '
                         '(much) less than 600.' in str(context.exception))
 
     def test_system_too_small(self):
         with self.assertRaises(AttributeError) as context:
-            util.System(2, 300, 2, mass=39.948, constants=[1.363e-134, 9.273e-78], forcefield=ff.lennard_jones)
+            util.System(2, 300, 2, mass=39.948,
+                        constants=[1.363e-134, 9.273e-78],
+                        forcefield=ff.lennard_jones)
         self.assertTrue('With a box length of 2 the cell is too small to '
                         'really hold more than one particle.' in str(
                                 context.exception))
 
     def test_system_init_conf(self):
         with self.assertRaises(NotImplementedError) as context:
-            util.System(2, 300, 100, init_conf='horseradish', mass=39.948, constants=[1.363e-134, 9.273e-78], forcefield=ff.lennard_jones)
+            util.System(2, 300, 100, init_conf='horseradish', mass=39.948,
+                        constants=[1.363e-134, 9.273e-78],
+                        forcefield=ff.lennard_jones)
         self.assertTrue('The initial configuration type horseradish is not '
                         'recognised. Available options are: square or '
                         'random' in str(context.exception))
