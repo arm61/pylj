cfb61a272ac77bed3384f67bb6ae43524e2804e3
Andrew McCluskey <arm61@users.noreply.github.com>
2019-05-08 13:56:25 +0200

    VERSION number bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ author = 'Andrew R. McCluskey'
 # built documents.
 #
 # The short X.Y version.
-version = '1.3.0'
+version = '1.3.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
