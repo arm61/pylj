d4172474d94d403dccc8c6f4bb08e066f06bb6fd
Andrew McCluskey <andrew.mccluskey@diamond.ac.uk>
2019-06-03 09:35:29 +0100

    bump version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ author = 'Andrew R. McCluskey'
 # built documents.
 #
 # The short X.Y version.
-version = '1.3.2'
+version = '1.3.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
